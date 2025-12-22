3f7b5fd35120f7616d81b2c1e1d9f3002c2041b8
Svetlana Zakharyan <svetlana.zakharyan@student.rau.am>
2025-12-22 12:04:27 +0400

    Update writer.cpp

--- Module2/writer.cpp
+++ Module2/writer.cpp
@@ -14,23 +14,26 @@ struct SharedData {
 
 int main() {
     int fd = shm_open("/my_shm", O_RDWR, 0666);
+    if (fd == -1) {
+        perror("shm_open failed");
+        return 1;
+    }
 
-    SharedData* data = (SharedData*)mmap(
-        nullptr,
-        sizeof(SharedData),
-        PROT_READ | PROT_WRITE,
-        MAP_SHARED,
-        fd,
-        0
-    );
+    SharedData* data = (SharedData*)mmap(nullptr, sizeof(SharedData),
+                                         PROT_READ | PROT_WRITE,
+                                         MAP_SHARED, fd, 0);
+    if (data == MAP_FAILED) {
+        perror("mmap failed");
+        return 1;
+    }
 
     pthread_mutex_lock(&data->mutex);
-
     std::strcpy(data->message, "Hello from writer process!");
     data->ready = true;
-
     pthread_cond_signal(&data->cond);
     pthread_mutex_unlock(&data->mutex);
+
+    std::cout << "Message written and signal sent.\n";
 
     munmap(data, sizeof(SharedData));
     return 0;
