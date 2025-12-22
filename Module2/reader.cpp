0e9d0a0e3a604486f9f1bc5fba1eb6297cf4da16
Svetlana Zakharyan <svetlana.zakharyan@student.rau.am>
2025-12-22 12:04:50 +0400

    Update reader.cpp

--- Module2/reader.cpp
+++ Module2/reader.cpp
@@ -13,25 +13,27 @@ struct SharedData {
 
 int main() {
     int fd = shm_open("/my_shm", O_RDWR, 0666);
+    if (fd == -1) {
+        perror("shm_open failed");
+        return 1;
+    }
 
-    SharedData* data = (SharedData*)mmap(
-        nullptr,
-        sizeof(SharedData),
-        PROT_READ | PROT_WRITE,
-        MAP_SHARED,
-        fd,
-        0
-    );
+    SharedData* data = (SharedData*)mmap(nullptr, sizeof(SharedData),
+                                         PROT_READ | PROT_WRITE,
+                                         MAP_SHARED, fd, 0);
+    if (data == MAP_FAILED) {
+        perror("mmap failed");
+        return 1;
+    }
 
     pthread_mutex_lock(&data->mutex);
-
     while (!data->ready) {
         pthread_cond_wait(&data->cond, &data->mutex);
     }
 
     std::cout << "Received: " << data->message << std::endl;
-
     pthread_mutex_unlock(&data->mutex);
+
     munmap(data, sizeof(SharedData));
     return 0;
 }
