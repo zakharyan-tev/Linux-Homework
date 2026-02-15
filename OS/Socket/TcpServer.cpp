48feac202d7e6015a3251aceddef448bb7d6b716
Svetlana Zakharyan <svetlana.zakharyan@student.rau.am>
2026-02-16 00:44:31 +0400

    Modify TcpServer to accept thread pool size
    Refactor TcpServer to use a thread pool for handling clients.

--- OS/Socket/TcpServer.cpp
+++ OS/Socket/TcpServer.cpp
@@ -3,17 +3,23 @@
 
 namespace SimpleNet {
 
-TcpServer::TcpServer(int port) {
+TcpServer::TcpServer(int port, size_t threads)
+    : pool_(threads)
+{
     listen_socket_.bind(port);
     listen_socket_.listen();
 }
 
+
 void TcpServer::run(ClientHandler handler) {
     while (true) {
         Socket client = listen_socket_.accept();
-        std::thread([handler, client = std::move(client)]() mutable {
-            handler(std::move(client));
-        }).detach();
+
+        pool_.enqueue(
+            [handler, client = std::move(client)]() mutable {
+                handler(std::move(client));
+            }
+        );
     }
 }
 
