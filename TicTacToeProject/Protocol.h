51fd93772c04f12fb667e9d71f4446e0a76b531f
Svetlana Zakharyan <svetlana.zakharyan@student.rau.am>
2026-03-29 21:15:31 +0400

    Updated

--- TicTacToeProject/Protocol.h
+++ TicTacToeProject/Protocol.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
 
-// Protocol: All messages are newline-terminated strings
 // Client -> Server:
 //   MOVE <position> (position 0-8)
 //   QUIT
@@ -16,13 +15,13 @@
 //   BOARD (pretty-printed board follows)
 
 namespace Protocol {
-    const std::string WELCOME    = "WELCOME";
-    const std::string STATE      = "STATE";
-    const std::string MOVE_CMD   = "MOVE";
-    const std::string MOVE_OK    = "MOVE_OK";
-    const std::string MOVE_ERR   = "MOVE_ERR";
-    const std::string WAITING    = "WAITING";
-    const std::string QUIT_CMD   = "QUIT";
+    const std::string WELCOME = "WELCOME";
+    const std::string STATE = "STATE";
+    const std::string MOVE_CMD = "MOVE";
+    const std::string MOVE_OK = "MOVE_OK";
+    const std::string MOVE_ERR = "MOVE_ERR";
+    const std::string WAITING = "WAITING";
+    const std::string QUIT_CMD = "QUIT";
     const std::string DISCONNECTED = "OPPONENT_DISCONNECTED";
 
     std::string buildWelcome(char symbol, int sessionId);
